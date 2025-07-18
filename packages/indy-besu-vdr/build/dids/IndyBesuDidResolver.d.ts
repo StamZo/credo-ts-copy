1cad092f8d55a7e46204391ade591814925977d4
StamZo <112801706+StamZo@users.noreply.github.com>
2025-07-18 15:56:17 +0300

    Delete packages/indy-besu-vdr directory

--- packages/indy-besu-vdr/build/dids/IndyBesuDidResolver.d.ts
+++
@@ -1,7 +0,0 @@
-import { AgentContext, DidResolutionResult, DidResolver } from '@credo-ts/core';
-export declare class IndyBesuDidResolver implements DidResolver {
-    readonly supportedMethods: string[];
-    readonly allowsCaching = false;
-    resolve(agentContext: AgentContext, did: string): Promise<DidResolutionResult>;
-    private updateContext;
-}
--- packages/indy-besu-vdr/build/ledger/IndyBesuSigner.d.ts
+++
@@ -1,7 +0,0 @@
-import { Transaction } from 'indy2-vdr';
-export declare class IndyBesuSigner {
-    readonly address: string;
-    private readonly signingKey;
-    constructor(secretKey: Uint8Array);
-    signTransaction(transaction: Transaction): void;
-}
